cf83ee616f39b0c84a5e9b6091a98455f3a9d139
umar-khan-1 <umarkhandev9@gmail.com>
2024-08-09 15:22:21 +0530

    Changes in the slider

--- src/components/protfolio.jsx
+++ src/components/protfolio.jsx
@@ -1,65 +1,50 @@
 import React, { forwardRef } from 'react';
-
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-// import { EffectCreative } from 'swiper/modules';
-import { Pagination } from "swiper/modules";
-// Import Swiper styles
-import "swiper/css";
-
-import { portfolio } from "../constant/data";
-import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import { portfolio } from '../constant/data';
+import { Link } from 'react-router-dom';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import "./portfolio.css"
 
 // eslint-disable-next-line react/display-name
 const Portfolio = forwardRef((props, ref) => {
   return (
     <div className="relative py-12 md:py-28 border-t overflow-hidden" ref={ref} {...props}>
-      <div className="mx-auto max-w-7xl px-6 md:px-12 lg:px-6 xl:px-0 ">
+      <div className="mx-auto max-w-7xl px-6 md:px-12 lg:px-6 xl:px-0">
         <div className="flex justify-between items-end">
-          <h3 className=" text-3xl md:xl:text-4xl xl:text-4xl font-black">
-            {/* Project<br /> */}
+          <h3 className="text-3xl md:text-4xl xl:text-4xl font-black">
             <span className="text-orange-600">Projects</span>
           </h3>
           <div className="flex">
-            <Link to='/projects' className="btn btn-orange">See All</Link>
+            <Link to="/projects" className="btn btn-orange">See All</Link>
           </div>
         </div>
       </div>
-      <div className="mt-8 md:mt-20">
+      <div className="mt-8 md:mt-20 px-4">
         <Swiper
-          slidesPerView={2}
-          loop={true}
-          centeredSlides={true}
-          spaceBetween={30}
-          grabCursor={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
+          spaceBetween={50}
+          slidesPerView={1} // Default value for mobile
+          pagination={{ clickable: true }} // Ensure pagination is clickable
+          modules={[Pagination]}
           breakpoints={{
             640: {
-              slidesPerView: 2,
+              slidesPerView: 1,
               spaceBetween: 20,
             },
             768: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-            1024: {
-              centeredSlides: true,
-              slidesPerView: 4,
+              slidesPerView: 2,
               spaceBetween: 30,
             },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
           }}
-          modules={[Pagination]}
-          className="mySwiper"
         >
           {portfolio.map((item, index) => (
             <SwiperSlide key={index}>
-              <div className=" bg-white rounded-xl overflow-hidden">
+              <div className="bg-white rounded-xl overflow-hidden">
                 <div>
                   <img className="w-full" src={item.img} alt="esoft" />
                 </div>
@@ -70,12 +55,11 @@ const Portfolio = forwardRef((props, ref) => {
               </div>
             </SwiperSlide>
           ))}
-
         </Swiper>
       </div>
       <div className="h-3/4 absolute w-full bg-gradient-to-r from-cyan-100 opacity-25 to-indigo-400 blur-[300px] dark:to-indigo-600 top-2/4"></div>
-
     </div>
   );
 });
+
 export default Portfolio;
